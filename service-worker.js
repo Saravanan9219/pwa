e806f813a2a2813033f9124d509cc98e40a2009b
Saravanan N <saravanan.n@happyfox.com>
2018-02-04 18:39:13 +0530

    rm test.html

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,6 @@ self.addEventListener('install', function(event){
         caches.open('v1').then(function(cache){
             return cache.addAll([
                 './bundle.js',
-                './test.html',
                 './index.html'
             ])
         })
